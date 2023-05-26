6dfce08bc755730aeda978ec4dda51aed0e99358
Vitor Lach <vitu@yomumangas.com>
2023-05-26 01:16:18 -0400

    fix(fetcher): change from node-fetch to axios

--- src/controllers/images.ts
+++ src/controllers/images.ts
@@ -1,21 +1,21 @@
 import { QuerySchema } from '@schemas/query';
+import axios from 'axios';
 import type { Request, Response } from 'express';
-import fetch from 'node-fetch';
 import sharp from 'sharp';
 
 export default async function ProcessImage(req: Request, res: Response) {
     try {
-        const { h, q, w, url, format } = req.query as Zod.TypeOf<typeof QuerySchema>;
-        const fetched = await fetch(url as string);
-        if (!fetched.ok) throw new Error('Failed to fetch image from url');
-        if (!fetched.headers.get('content-type')?.startsWith('image/'))
+        const { h, q, w, url, format } = req.query as unknown as Zod.TypeOf<typeof QuerySchema>;
+        const fetched = await axios.get(url, { responseType: 'arraybuffer' }).catch(() => null);
+        if (!fetched) throw new Error('Failed to fetch image from url');
+        if (!`${fetched.headers['Content-Type']}`?.startsWith('image/'))
             throw new Error('Provided URL is not from a image');
-        const image = sharp(await fetched.arrayBuffer());
+        const image = sharp(await fetched.data());
         if (w || h) image.resize(w, h);
         return res
             .setHeader('Content-Type', `image/${format}`)
             .send(await image.toFormat(format as 'png', { quality: q }).toBuffer());
-    } catch (err) {
+    } catch (err: any) {
         return res.status(500).send({ status: 500, message: err.message });
     }
 }
